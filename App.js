3eb7fe35a861e39b7d7773a764ed7c9f83dcf6e2
juliaathar <juathar@gmail.com>
2024-01-25 09:25:07 -0300

    :sparkles: Estrutura de pastas e input
    Feito estrutura de pastas do projeto e adicionado input pra estudos de estilizaçao

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import { TextInput } from 'react-native-web';
 
 const styles = StyleSheet.create({
   container: {
     paddingTop: 50,
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#232121',
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -16,18 +17,33 @@ const styles = StyleSheet.create({
   text:{
     fontWeight:'700',
     fontSize: 30,
+    color: 'white'
+  },
+  input: {
+    width: '80%',
+    height: 40,
+    borderWidth: 2,
+    borderRadius: 5,
+    borderColor: 'white',
+    backgroundColor: 'rgba(255, 255, 255, 0.5)',
+    padding: 10,
+    color: 'white'
   }
 });
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style = {styles.text}>Hello, World!</Text>
       <Image
         style={styles.foto}
         source={{
           uri: 'https://a.espncdn.com/combiner/i?img=/i/teamlogos/soccer/500/874.png',
         }}
+      />
+      <Text style = {styles.text}>Hello, World!</Text>
+      <TextInput
+        style = {styles.input}
+        placeholder= "example..."
       />
       <StatusBar style="auto" />
     </View>
