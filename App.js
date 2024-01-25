718db7a6aea83881b8f13ea2088934e7f62b61da
juliaathar <juathar@gmail.com>
2024-01-25 08:40:21 -0300

    :tada: init

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: 50,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  foto: {
+    width: 200,
+    height: 200,
+  },
+  text:{
+    fontWeight:'700',
+    fontSize: 30,
+  }
 });
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style = {styles.text}>Hello, World!</Text>
+      <Image
+        style={styles.foto}
+        source={{
+          uri: 'https://a.espncdn.com/combiner/i?img=/i/teamlogos/soccer/500/874.png',
+        }}
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
